6954b0ee28e5063154e0b7ab99ab9acdffe5ea7d
Victor <victormaciel28@hotmail.com>
2018-06-16 08:24:09 -0300

    Mandando dois numeros para o php e recebendo a soma

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ $(function(){
     $(form).bind('submit', function(e){
         e.preventDefault();
 
-        var txt=$(this).serialize();
+        var txt= $(this).serialize();
         console.log(txt);
+
+        $.ajax({
+            type:'GET',
+            url:'ajax.php',
+            data:txt,
+            success:function(resultado) {
+                $('.div').html("Resultado "+resultado);
+            },
+            error:function(){
+                alert("Deu algum erro.");
+            }
+        });
     });
 
 });
