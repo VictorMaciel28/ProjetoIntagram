e211d470d59f04648a30540874085c41ba1b04a7
Victor <victormaciel28@hotmail.com>
2018-06-19 15:14:25 -0300

    Usando GET para obter Json do Instagram

--- ajax.php
+++
@@ -1,5 +0,0 @@
-<?php
-$x = intval($_GET['x']);
-$y = intval($_GET['y']);
-echo $x+$y;
-?>
--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-$(function(){
-   
-    $(form).bind('submit', function(e){
-        e.preventDefault();
-
-        var txt= $(this).serialize();
-        console.log(txt);
-
-        $.ajax({
-            type:'GET',
-            url:'ajax.php',
-            data:txt,
-            success:function(resultado) {
-                $('.div').html("Resultado "+resultado);
-            },
-            error:function(){
-                alert("Deu algum erro.");
-            }
-        });
+window.Comentarios = {
+    // Credenciais , Settings
+    config:{},
+    
+    getComments: function(){
+       var URL = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=859867556.1677ed0.f05bf2fc29444a568ed673012397e6ff';
+       $.ajax({
+        type:'GET',
+        url: URL,
+        data:'json',
+        success:function(response) {
+            console.log(response);
+        },
+        error:function(){
+            alert("Deu algum erro.");
+        }
     });
+    },
+};
 
-});
+
+
