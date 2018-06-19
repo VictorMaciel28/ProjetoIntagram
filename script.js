6129af9f59e23de733d689ab2cbf6f3d1bfc7ee7
Victor <victormaciel28@hotmail.com>
2018-06-19 19:15:14 -0300

    Adicao de package.json e simplificacao do codigo

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-window.Comentarios = {
-    // Credenciais , Settings
-    config:{},
-    
-    getComments: function(){
+window.Legendas = {
+    getLegendas: function(){
        var URL = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=859867556.1677ed0.f05bf2fc29444a568ed673012397e6ff';
        $.ajax({
-        type:'GET',
-        url: URL,
-        data:'json',
-        success:function(response) {
-            console.log(response);
-        },
-        error:function(){
+            type:'GET',
+            url: URL,
+            data:'jsonp',
+            success:function(response) {
+                i=0;
+                legendas = [];
+                for (i=0;i<20;i++){
+                    legendas[i] = response.data[i].caption.text;
+                }
+            console.log(legendas);
+            },
+            error:function(){
             alert("Deu algum erro.");
-        }
-    });
+            }
+        });
     },
+    
 };
+
 
 
 
